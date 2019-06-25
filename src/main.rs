6c47ef89518783d2bc8970b1a03cf2ffa7fd6032
Quintin Andrew Castillo <quintin.castillo@nyu.edu>
2019-06-25 19:51:05 -0400

    hola, rust by example fmt::Display

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,64 @@
-#[derive(Debug)]
-struct Structure(i32);
+use std::fmt;
 
 #[derive(Debug)]
-struct Deep(Structure);
+struct MinMax(i64, i64);
 
-#[derive(Debug)]
-struct Person<'a> {
-    name: &'a str,
-    age: u8,
+impl fmt::Display for MinMax {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "({}, {})", self.0, self.1)
+    }
 }
+
+#[derive(Debug)]
+struct Point2D {
+    x: f64,
+    y: f64,
+}
+
+impl fmt::Display for Point2D {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "x: {}, y: {}", self.x, self.y)
+    }
+}
+
+#[derive(Debug)]
+struct Complex {
+    real: f64,
+    imag: f64,
+}
+
+impl fmt::Display for Complex {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} + {}i", self.real, self.imag)
+    }
+}
+
 
 fn main() {
-    // Printing with '{:?}' is similar to with '{}'
-    println!("{:?} months in a year.", 12);
-    println!("{1:?} {0:?} is the {actor:?} name.",
-             "Schwarzeneggrolls",
-             "AhhnohlDD",
-             actor = "gubernator's");
+    let minmax = MinMax(0, 14);
 
-    println!("Now {:?} will print", Structure(3));
-    println!("Now the deep {:?} shall print!", Deep(Structure(32)));
+    println!("Compare structures:");
+    println!("Display: {}", minmax);
+    println!("Debug: {:?}", minmax);
 
-    // Pretty printing
-    let name = "Mary";
-    let age = 17;
-    let madonna = Person { name, age };
+    let big_range = MinMax(-300, 300);
+    let small_range = MinMax(-3, 3);
 
-    println!("Pretty name, {:#?}", madonna);
+    println!("The big range is {big} and the small is {small}",
+             small = small_range,
+             big = big_range);
+
+    let point = Point2D { x: 3.3, y: 7.2 };
+
+    println!("Compare points:");
+    println!("Display: {}", point);
+    println!("Debug: {:?}", point);
+
+    let complex = Complex { real: 3.3, imag: 7.2 };
+
+    println!("Display: {}", complex);
+    println!("Debug: {:?}", complex);
 }
+
+
 
