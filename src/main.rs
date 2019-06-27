ee9a2d80e239d969d2fbb2a6c8a166bed62df942
Quintin Andrew Castillo <quintin.castillo@nyu.edu>
2019-06-26 20:36:10 -0400

    hola, rust by example fmt::Display

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,3 @@
-use std::fmt;
-
-#[derive(Debug)]
-struct MinMax(i64, i64);
-
-impl fmt::Display for MinMax {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "({}, {})", self.0, self.1)
-    }
-}
-
-#[derive(Debug)]
-struct Point2D {
-    x: f64,
-    y: f64,
-}
-
-impl fmt::Display for Point2D {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "x: {}, y: {}", self.x, self.y)
-    }
-}
-
-#[derive(Debug)]
-struct Complex {
-    real: f64,
-    imag: f64,
-}
-
-impl fmt::Display for Complex {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{} + {}i", self.real, self.imag)
-    }
-}
-
-
 fn main() {
     let minmax = MinMax(0, 14);
 
@@ -61,4 +25,38 @@ fn main() {
 }
 
 
+use std::fmt;
+
+impl fmt::Display for Point2D {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "x: {}, y: {}", self.x, self.y)
+    }
+}
+
+#[derive(Debug)]
+struct Point2D {
+    x: f64,
+    y: f64,
+}
+
+impl fmt::Display for MinMax {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "({}, {})", self.0, self.1)
+    }
+}
+
+#[derive(Debug)]
+struct MinMax(i64, i64);
+
+impl fmt::Display for Complex {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} + {}i", self.real, self.imag)
+    }
+}
+
+#[derive(Debug)]
+struct Complex {
+    real: f64,
+    imag: f64,
+}
 
